a840c97b9c16cedba5bb31d22146f20dce0a7103
Aurot1013 <aurelien.berlot1013@gmail.com>
2026-01-13 14:12:56 +0100

    LandingPage
    Creation LandingPages without navbar

--- clients/web/src/App.jsx
+++ clients/web/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import { LandingPage } from './pages/LandingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Par défaut, on affiche la page Register */}
-        <Route path="/" element={<Register />} />
+        {/* Par défaut, on affiche la page d'accueil */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/LandingPage" element={<LandingPage />} />
+        {/* Route pour la page Register */}
         <Route path="/register" element={<Register />} />
         {/* Route pour la page Login */}
         <Route path="/login" element={<Login />} />
